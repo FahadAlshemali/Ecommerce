50b1cb66aaa8db7c980940250b16fe039bd1e5df
FahadAlshemali <fhadfhad222@gmail.com>
2022-08-25 13:11:16 +0200

    setup API

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -1,14 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//  Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { extractSheets } from 'spreadsheet-to-json'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+// interface Data extends GaxiosResponse{
+//   name: string
+// }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  
-  res.status(200).json({ name: 'John Doe' })
+  extractSheets(
+    {
+      // your google spreadhsheet key
+      spreadsheetKey: '12ozPThNSYrW6D16tg0zabyZqEsRT7y4XjB5nsJkHNmM',
+      // your google oauth2 credentials or API_KEY
+      credentials: require('../../credentials.json'),
+      // optional: names of the sheets you want to extract
+      sheetsToExtract: ['Product'],
+      // optional: custom function to parse the cells
+      //formatCell: formatCell
+    },
+    function (err: any, data: any) {
+      console.log('Product: ', data.Product)
+      res.send(data.Product)
+    }
+  )
 }
